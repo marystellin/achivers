4032a51f322ef01ab5551b3ada56b5f3cf3076d7
marystellin <53898753+marystellin@users.noreply.github.com>
2019-08-14 17:17:52 +0530

    odd.c

---
+++ odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	
+	int a;
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("even");
+	}
+	else if("a%2!=0")
+	{
+		printf("odd");
+	}
+	else
+	{
+		printf("invalid");
+	}
+	return 0;
+}
